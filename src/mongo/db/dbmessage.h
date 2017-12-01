8dc20041b86155dda8931646d83fafc4cb9dce76
suytt <simon@busit.com>
2017-12-01 23:46:06 +0100

    first attempt in type restriction

--- src/mongo/db/dbmessage.h
+++ src/mongo/db/dbmessage.h
@@ -253,7 +253,7 @@ public:
     BSONObj nextJsObj();
 	
 	BSONObj restrictNativeTypes(const BSONObj& bson);
-	BSONObj DbMessage::restrictNativeTypes_recursive(mongo::mutablebson::Document& doc);
+	BSONObj DbMessage::restrictNativeTypes_recursive(mongo::mutablebson::Element& parent);
 
     const Message& msg() const {
         return _msg;
