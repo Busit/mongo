2a5a7c0766e6e9731e268afd85da1ddaabbad9e2
suytt <simon@busit.com>
2017-12-03 20:04:20 +0100

    debug

--- src/mongo/db/commands/dbcommands.cpp
+++ src/mongo/db/commands/dbcommands.cpp
@@ -1505,7 +1505,7 @@ bool Command::run(OperationContext* txn,
 
     // run expects non-const bsonobj
     BSONObj cmd = request.getCommandArgs();
-	log() << "DEBUG COMMAND RUN POINT";
+	log() << "DEBUG COMMAND RUN POINT: " << cmd;
 
     // run expects const db std::string (can't bind to temporary)
     const std::string db = request.getDatabase().toString();
